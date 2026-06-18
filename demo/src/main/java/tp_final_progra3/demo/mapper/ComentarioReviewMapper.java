15914b94f028a68f23d3aacac56743adbd5b2716
renatag7 <renigarabito@gmail.com>
2026-06-18 20:54:01 -0300

    /funcionan comentarios

--- demo/src/main/java/tp_final_progra3/demo/mapper/ComentarioReviewMapper.java
+++ demo/src/main/java/tp_final_progra3/demo/mapper/ComentarioReviewMapper.java
@@ -1,6 +1,7 @@
 package tp_final_progra3.demo.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import tp_final_progra3.demo.model.dto.request.ComentarioReviewRequestDTO;
 import tp_final_progra3.demo.model.dto.request.RegisterRequestDTO;
 import tp_final_progra3.demo.model.dto.response.ComentarioReviewResponseDTO;
@@ -11,5 +12,7 @@ import tp_final_progra3.demo.model.entity.Usuario;
 @Mapper(componentModel = "spring")
 public interface ComentarioReviewMapper {
     ComentarioReview toEntity(ComentarioReviewRequestDTO comentarioReviewRequestDTO);
+
+    @Mapping(target = "username", source = "usuario.username")
     ComentarioReviewResponseDTO toDTO(ComentarioReview comentarioReview);
 }
--- target/generated-sources/annotations/tp_final_progra3/demo/mapper/ComentarioReviewMapperImpl.java
+++ target/generated-sources/annotations/tp_final_progra3/demo/mapper/ComentarioReviewMapperImpl.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 import tp_final_progra3.demo.model.dto.request.ComentarioReviewRequestDTO;
 import tp_final_progra3.demo.model.dto.response.ComentarioReviewResponseDTO;
 import tp_final_progra3.demo.model.entity.ComentarioReview;
+import tp_final_progra3.demo.model.entity.Usuario;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-18T19:26:12-0300",
+    date = "2026-06-18T20:09:40-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 @Component
@@ -34,18 +35,26 @@ public class ComentarioReviewMapperImpl implements ComentarioReviewMapper {
             return null;
         }
 
+        String username = null;
         Long id = null;
         String contenido = null;
         LocalDate fechaPublicado = null;
 
+        username = comentarioReviewUsuarioUsername( comentarioReview );
         id = comentarioReview.getId();
         contenido = comentarioReview.getContenido();
         fechaPublicado = comentarioReview.getFechaPublicado();
 
-        String username = null;
-
         ComentarioReviewResponseDTO comentarioReviewResponseDTO = new ComentarioReviewResponseDTO( id, contenido, username, fechaPublicado );
 
         return comentarioReviewResponseDTO;
+    }
+
+    private String comentarioReviewUsuarioUsername(ComentarioReview comentarioReview) {
+        Usuario usuario = comentarioReview.getUsuario();
+        if ( usuario == null ) {
+            return null;
+        }
+        return usuario.getUsername();
     }
 }
